572a7604cb6c5ae7a9356c60207ebd567a989e87
meghana <meghanachowdaryvemulapalli1@gmail.com>
2024-06-12 12:35:30 +0530

    adding commnet in 01_project installing jenkins software in ubantu

--- PROJECT /01_PROJECT /install_ubantu.sh
+++ PROJECT /01_PROJECT /install_ubantu.sh
@@ -1,5 +1,8 @@
+# OPERATING SYSTEM : LINUX  | DISTRO: UBANTU 
 sudo apt update
+# This command will install both maven and java 
 sudo apt install maven -y
+# This command is used to install jenkins 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
